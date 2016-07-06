494fa667e6b1273f11988f1710ecbb889d0a45ea
GabeQ <gquiroz@hmc.edu>
2016-07-06 15:39:53 -0700

    fields added

--- Jane/src/edu/hmc/jane/solving/LowerBound.java
+++ Jane/src/edu/hmc/jane/solving/LowerBound.java
@@ -5,10 +5,21 @@
  */
 package edu.hmc.jane.solving;
 
+import edu.hmc.jane.Tree;
+import edu.hmc.jane.Phi;
+
 /**
  *
  * @author cssummer16
  */
 public class LowerBound {
     
-}
+    public long Infinity = Long.MAX_VALUE;
+    public Tree HostTree;
+    public Tree ParaTree;
+    public Phi Phi;
+    public static int dval;
+    public static int lval;
+    public static int tval;
+    
+            }
