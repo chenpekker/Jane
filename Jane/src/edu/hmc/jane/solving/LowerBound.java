98becb99ab2435a954e8ad3946b98722ad9c156e
GabeQ <gquiroz@hmc.edu>
2016-07-06 14:54:14 -0700

    Lower Bound Class

---
+++ Jane/src/edu/hmc/jane/solving/LowerBound.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hmc.jane.solving;
+
+/**
+ *
+ * @author cssummer16
+ */
+public class LowerBound {
+    
+}
